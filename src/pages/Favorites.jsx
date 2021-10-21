2d59d2439544ebebcacbafddb0e4de77ce737c78
David Souza <dabcs@poli.ufrj.br>
2021-10-21 15:52:29 -0300

    requisito 12

--- src/pages/Favorites.jsx
+++ src/pages/Favorites.jsx
@@ -1,12 +1,79 @@
 import React, { Component } from 'react';
 import Header from '../components/Header';
+import { getFavoriteSongs, removeSong } from '../services/favoriteSongsAPI';
+import MusicCard from '../components/MusicCard';
+import Loading from './Loading';
 
 export default class Favorites extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      musicsList: [],
+      loading: false,
+    };
+    this.getMusicsList = this.getMusicsList.bind(this);
+    this.handleLoading = this.handleLoading.bind(this);
+    this.handleCheckbox = this.handleCheckbox.bind(this);
+    this.getFavorites = this.getFavorites.bind(this);
+  }
+
+  componentDidMount() {
+    this.getMusicsList();
+  }
+
+  async handleLoading(loading) {
+    this.setState({
+      loading,
+    });
+  }
+
+  handleCheckbox(index) {
+    const { musicsList } = this.state;
+    const updateFavorite = [...musicsList];
+    removeSong(musicsList[index]);
+    updateFavorite.splice(index, 1);
+    this.setState({
+      musicsList: updateFavorite,
+    });
+  }
+
+  async getMusicsList() {
+    const musicsList = await getFavoriteSongs();
+    this.setState({
+      musicsList,
+    });
+    this.getFavorites();
+  }
+
+  async getFavorites() {
+    this.setState({ loading: true });
+    await getFavoriteSongs();
+    this.setState({ loading: false });
+  }
+
   render() {
+    const { musicsList, loading } = this.state;
+    const active = true;
     return (
       <div data-testid="page-favorites">
         <Header />
-        <p> Favorites </p>
+        {
+          loading ? <Loading /> : (
+            musicsList.map((music, index) => (
+              <MusicCard
+                key={ music.trackId }
+                trackName={ music.trackName }
+                previewUrl={ music.previewUrl }
+                trackId={ music.trackId }
+                musicObj={ musicsList[index] }
+                handleLoading={ this.handleLoading }
+                handleCheckbox={ this.handleCheckbox }
+                index={ index }
+                checked={ active }
+              />
+            ))
+          )
+        }
       </div>
     );
   }
