9a3d88400b0f70ac15e1a4c8c3f9daafdb0c236e
David Souza <dabcs@poli.ufrj.br>
2021-10-23 19:37:32 -0300

    tentativa de resolver ultimo requisito

--- src/pages/ProfileEdit.jsx
+++ src/pages/ProfileEdit.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import { Redirect } from 'react-router';
+import { Redirect, Switch } from 'react-router';
+import PropTypes from 'prop-types';
 import Header from '../components/Header';
 import Loading from './Loading';
 import { getUser, updateUser } from '../services/userAPI';
@@ -20,6 +21,7 @@ export default class ProfileEdit extends Component {
     this.handleChange = this.handleChange.bind(this);
     this.enableSubmitButton = this.enableSubmitButton.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.renderProfileEdit = this.renderProfileEdit.bind(this);
   }
 
   componentDidMount() {
@@ -37,8 +39,10 @@ export default class ProfileEdit extends Component {
   async handleSubmit(event) {
     event.preventDefault();
     const { inputName, inputEmail, imgSrc, inputDescription } = this.state;
+    const { history } = this.props;
     this.setState({
       loading: true,
+      redirect: true,
     });
     await updateUser({
       name: inputName,
@@ -46,9 +50,7 @@ export default class ProfileEdit extends Component {
       image: imgSrc,
       description: inputDescription,
     });
-    this.setState({
-      redirect: true,
-    });
+    history.push('/profile');
   }
 
   async getProfile() {
@@ -78,89 +80,101 @@ export default class ProfileEdit extends Component {
     }
   }
 
-  render() {
+  renderProfileEdit() {
     const { loading, inputName, inputEmail,
-      imgSrc, inputDescription, buttonDisable, redirect } = this.state;
+      imgSrc, inputDescription, buttonDisable } = this.state;
+    if (loading) {
+      return <Loading />;
+    }
+    return (
+      <section
+        className="profile-edit"
+      >
+        <form className="form-edit-profile">
+          <label className="form-group mt-lg-4" htmlFor="edit-input-name">
+            Nome:
+            <input
+              type="text"
+              name="inputName"
+              id="edit-input-name"
+              className="form-control"
+              data-testid="edit-input-name"
+              value={ inputName }
+              onChange={ this.handleChange }
+              required
+            />
+          </label>
+          <label className="form-group mt-lg-4" htmlFor="edit-input-email">
+            Email:
+            <input
+              type="email"
+              name="inputEmail"
+              id="edit-input-email"
+              className="form-control"
+              data-testid="edit-input-email"
+              value={ inputEmail }
+              onChange={ this.handleChange }
+              required
+            />
+          </label>
+          <label className="form-group mt-lg-4" htmlFor="edit-input-image">
+            Imagem:
+            <input
+              type="text"
+              name="imgSrc"
+              id="edit-input-image"
+              className="form-control"
+              data-testid="edit-input-image"
+              value={ imgSrc }
+              onChange={ this.handleChange }
+              required
+            />
+          </label>
+          <label className="form-group mt-lg-4" htmlFor="edit-input-description">
+            Descrição:
+            <textarea
+              type="text"
+              rows="5"
+              cols="23"
+              name="inputDescription"
+              id="edit-input-description"
+              className="form-control"
+              data-testid="edit-input-description"
+              value={ inputDescription }
+              onChange={ this.handleChange }
+              required
+            />
+            <button
+              disabled={ buttonDisable }
+              data-testid="edit-button-save"
+              type="submit"
+              className="btn btn-success"
+              onClick={ this.handleSubmit }
+            >
+              Editar perfil
+            </button>
+          </label>
+        </form>
+      </section>
+    );
+  }
+
+  render() {
+    const { redirect } = this.state;
     return (
       <div data-testid="page-profile-edit">
         <Header />
-        {
-          loading ? <Loading /> : (
-            <section
-              className="profile-edit"
-            >
-              <form className="form-edit-profile">
-                <label className="form-group mt-lg-4" htmlFor="edit-input-name">
-                  Nome:
-                  <input
-                    type="text"
-                    name="inputName"
-                    id="edit-input-name"
-                    className="form-control"
-                    data-testid="edit-input-name"
-                    value={ inputName }
-                    onChange={ this.handleChange }
-                    required
-                  />
-                </label>
-                <label className="form-group mt-lg-4" htmlFor="edit-input-email">
-                  Email:
-                  <input
-                    type="email"
-                    name="inputEmail"
-                    id="edit-input-email"
-                    className="form-control"
-                    data-testid="edit-input-email"
-                    value={ inputEmail }
-                    onChange={ this.handleChange }
-                    required
-                  />
-                </label>
-                <label className="form-group mt-lg-4" htmlFor="edit-input-image">
-                  Imagem:
-                  <input
-                    type="text"
-                    name="imgSrc"
-                    id="edit-input-image"
-                    className="form-control"
-                    data-testid="edit-input-image"
-                    value={ imgSrc }
-                    onChange={ this.handleChange }
-                    required
-                  />
-                </label>
-                <label className="form-group mt-lg-4" htmlFor="edit-input-description">
-                  Descrição:
-                  <textarea
-                    type="text"
-                    rows="5"
-                    cols="23"
-                    name="inputDescription"
-                    id="edit-input-description"
-                    className="form-control"
-                    data-testid="edit-input-description"
-                    value={ inputDescription }
-                    onChange={ this.handleChange }
-                    required
-                  />
-                  <button
-                    disabled={ buttonDisable }
-                    data-testid="edit-button-save"
-                    type="submit"
-                    className="btn btn-success"
-                    onClick={ this.handleSubmit }
-                  >
-                    Salvar
-                  </button>
-                </label>
-              </form>
-            </section>
-          )
-        }
-        {
-          redirect && <Redirect to="/profile" />
-        }
+        <Switch>
+          {
+            redirect ? <Redirect to="/profile" /> : this.renderProfileEdit()
+          }
+
+        </Switch>
       </div>
     );
   }
 }
+
+ProfileEdit.propTypes = ({
+  history: PropTypes.func.isRequired,
+});
